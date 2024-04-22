8e327c646992c0953a1ab219f223635330fb6440
Juan Diego Porras <jporras@iqtsystems.com>
2024-04-22 08:57:00 -0400

    finalizada implementacion del filtro de productos a nivel logico y de estilos

--- src/app/products/components/section-filter/section-filter.component.ts
+++ src/app/products/components/section-filter/section-filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FiltersStyle, ObjFilter } from 'src/app/shared/types/filterStatus';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-section-filter',
@@ -11,6 +12,9 @@ export class SectionFilterComponent {
   @Output() updateList = new EventEmitter<string>();
   filtersStyleList: ObjFilter = FiltersStyle;
   
+  constructor(private productsService: ProductsService) {
+
+  }
   
   ngOnInit() {
     
@@ -18,13 +22,42 @@ export class SectionFilterComponent {
 
   chooseFilter(filter: string) {
     this.updateList.emit(filter);
+
+
   }
 
-  get styles() {
-    if(this.filtersStyleList['active']) {
-      return this.filtersStyleList['active'];
+  get allFilterStyles() {
+    if(this.productsService.filter === "all") {
+      if(this.filtersStyleList['active']) {
+        return this.filtersStyleList['active'];
+      }
+      return {}
     }
-    return {}
+    else {
+      if(this.filtersStyleList['inactive']) {
+        return this.filtersStyleList['inactive'];
+      }
+      return {}
+    }
+
+    
+  }
+
+  get availableFilterStyles() {
+    if(this.productsService.filter === "available") {
+      if(this.filtersStyleList['active']) {
+        return this.filtersStyleList['active'];
+      }
+      return {}
+    }
+    else {
+      if(this.filtersStyleList['inactive']) {
+        return this.filtersStyleList['inactive'];
+      }
+      return {}
+    }
+    
+    
   }
 
  
